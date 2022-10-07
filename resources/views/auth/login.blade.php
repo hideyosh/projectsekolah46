c2c44ca3ebe88c060c2139e24e525a8664ab789a
hideyosh <buditriannggoro@gmail.com>
2022-10-07 13:23:16 +0700

    wip

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -55,6 +55,8 @@
         </form>
     </x-auth-card>
 </x-guest-layout> --}}
+
+
 <!DOCTYPE html>
 <html lang="en" class="h-100">
 
@@ -63,8 +65,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Acara - Ticketing Bootstrap Admin Dashboard</title>
+
+    {{-- Bootstrap CSS CDN --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('template/xhtml/images/favicon.png') }}">
+
     <link href="{{ asset('template/xhtml/css/style.css') }}" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 </head>
@@ -82,15 +89,15 @@
 										<a href="index.html"><img src="{{ asset('template/xhtml/images/logo-full.png') }}" alt=""></a>
 									</div>
                                     <h4 class="text-center mb-4 text-white">Sign in your account</h4>
-                                    <form action="index.html">
-                                        <div class="form-group">
-                                            <label class="mb-1 text-white"><strong>Email</strong></label>
-                                            <input type="email" class="form-control" value="hello@example.com">
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="mb-1 text-white"><strong>Password</strong></label>
-                                            <input type="password" class="form-control" value="Password">
-                                        </div>
+                                    <form action="{{ route('login') }}" method="POST">
+                                        <div class="form-floating mb-3">
+                                            <input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com" required>
+                                            <label for="floatingInput">Email address</label>
+                                          </div>
+                                          <div class="form-floating">
+                                            <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
+                                            <label for="floatingPassword">Password</label>
+                                          </div>
                                         <div class="form-row d-flex justify-content-between mt-4 mb-2">
                                             <div class="form-group">
                                                <div class="custom-control custom-checkbox ml-1 text-white">
@@ -106,7 +113,7 @@
                                             <button type="submit" class="btn bg-white text-primary btn-block">Sign Me In</button>
                                         </div>
                                     </form>
-                                    <div class="new-account mt-3">
+                                    <div class="new-account mt-3 text-center">
                                         <p class="text-white">Don't have an account? <a class="text-white" href="{{ route('register') }}">Sign up</a></p>
                                     </div>
                                 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -70,6 +70,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Acara - Ticketing Bootstrap Admin Dashboard</title>
+
+    {{-- Bootstrap CSS CDN --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('template/xhtml/images/favicon.png') }}">
     <link href="{{ asset('template/xhtml/css/style.css') }}" rel="stylesheet">
@@ -90,24 +94,38 @@
 										<a href="index.html"><img src="{{ asset('template/xhtml/images/logo-full.png') }}" alt=""></a>
 									</div>
                                     <h4 class="text-center mb-4 text-white">Sign up your account</h4>
-                                    <form action="index.html">
-                                        <div class="form-group">
-                                            <label class="mb-1 text-white"><strong>Username</strong></label>
-                                            <input type="text" class="form-control" placeholder="username">
+                                    <form action="{{ route('register') }}" method="POST">
+                                        @if ($errors->any())
+                                        <div class="alert alert-danger">
+                                            <ul>
+                                                @foreach ($errors->all() as $error)
+                                                    <li>{{ $error }}</li>
+                                                @endforeach
+                                            </ul>
                                         </div>
-                                        <div class="form-group">
-                                            <label class="mb-1 text-white"><strong>Email</strong></label>
-                                            <input type="email" class="form-control" placeholder="hello@example.com">
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="mb-1 text-white"><strong>Password</strong></label>
-                                            <input type="password" class="form-control" value="Password">
-                                        </div>
+                                        @endif
+                                        @csrf
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="floatingInput" name="name" placeholder="name@example.com" required>
+                                            <label for="floatingInput">Username</label>
+                                          </div>
+                                          <div class="form-floating mb-3">
+                                            <input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com" required>
+                                            <label for="floatingInput">Email address</label>
+                                          </div>
+                                          <div class="form-floating mb-3">
+                                            <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
+                                            <label for="floatingPassword">Password</label>
+                                          </div>
+                                        <div class="form-floating ">
+                                            <input type="password" class="form-control" id="floatingPassword" name="password_confirmation" placeholder="Password">
+                                            <label for="Confrim-Password">Confirm Password</label>
+                                          </div>
                                         <div class="text-center mt-4">
                                             <button type="submit" class="btn bg-white text-primary btn-block">Sign me up</button>
                                         </div>
                                     </form>
-                                    <div class="new-account mt-3">
+                                    <div class="new-account mt-3 text-center">
                                         <p class="text-white">Already have an account? <a class="text-white" href="{{ route('login') }}">Sign in</a></p>
                                     </div>
                                 </div>
