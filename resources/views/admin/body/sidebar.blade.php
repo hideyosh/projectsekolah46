1bf35df18875d54f07fe9147939e0873c39282a5
dindazahrars <99201657+dindazahrars@users.noreply.github.com>
2022-10-09 18:30:16 +0700

    .

---
+++ resources/views/admin/body/sidebar.blade.php
@@ -0,0 +1,33 @@
+<div class="deznav">
+    <div class="deznav-scroll">
+        <ul class="metismenu" id="menu">
+            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-381-networking"></i>
+                    <span class="nav-text">Admin & User</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="index.html">Admin</a></li>
+                    <li><a href="event.html">User</a></li>
+                </ul>
+            </li>
+            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-381-television"></i>
+                    <span class="nav-text">Apps</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="./ecom-customers.html">All</a></li>
+                    <li><a href="./ecom-product-grid.html">ATK</a></li>
+                    <li><a href="./ecom-product-list.html">Kebersihan</a></li>
+                    <li><a href="./ecom-product-detail.html">Elektro</a></li>
+                    <li><a href="./ecom-product-order.html">Alat Listrik</a></li>
+                    <li><a href="./ecom-checkout.html">Alat Olahraga</a></li>
+                    <li><a href="./ecom-invoice.html">Lain-Lain</a></li>
+                </ul>
+            </li>
+        </ul>
+        <div class="copyright">
+            <p><strong>Acara Ticketing Dashboard</strong> © 2021 All Rights Reserved</p>
+            <p>Made with <span class="heart"></span> by DexignZone</p>
+        </div>
+    </div>
+</div>
